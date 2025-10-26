da55cc7ed158fb89976a02b2a34241d8e2445de6
joinmouse <joinmouse@foxmail.com>
2025-10-26 13:02:13 +0800

    feat: 完善解除质押的方法和单元测试

--- stake-contract/contracts/MetaNodeStake.sol
+++ stake-contract/contracts/MetaNodeStake.sol
@@ -9,10 +9,10 @@ contract MetaNodeStake {
     uint256 public totalStaked; // 全局总质押ETH
     mapping(address => uint256) public userStake; // 用户质押量
 
-    // 事件：质押行为
-    event Staked(address indexed user, uint256 amount);
+    event Staked(address indexed user, uint256 amount);  // 质押事件
+    event Unstaked(address indexed user, uint256 amount); // 取消质押事件
 
-    // 质押函数，用户调用此函数进行质押
+    // 1、质押函数，用户调用此函数进行质押
     function stake() external payable {
         require(msg.value > 0, "Stake amount must be greater than zero");
 
@@ -21,5 +21,21 @@ contract MetaNodeStake {
         totalStaked += msg.value;
 
         emit Staked(msg.sender, msg.value);
+    }
+
+    // 2、取消质押函数，用户调用此函数取消质押
+    function unstake(uint256 amount) external {
+        require(amount > 0, "Unstake amount must be greater than zero");
+        require(userStake[msg.sender] >= amount, "Insufficient staked amount");
+
+        // 更新用户质押量和全局总质押量
+        userStake[msg.sender] -= amount;
+        totalStaked -= amount;
+
+        // 将ETH发送回用户
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Unstake: transfer failed");
+
+        emit Unstaked(msg.sender, amount);
     }
 }
